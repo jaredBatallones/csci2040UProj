cc78e54f5d8bf16dd0e78eeaf108f04c1e4ddc3b
jilunliang <91205234+jilunliang@users.noreply.github.com>
2025-01-25 02:57:56 +0800

    Revert "Merge branch 'main' of https://github.com/jaredBatallones/csci2040UProj"
    This reverts commit 672dfd7dcd2f0afc6617996d5dcc163bd9d2a189, reversing
    changes made to 19f0053d01fd4fa01678c951e959cd1555fe88fc.

--- Lab02/Front_end.py
+++ Lab02/Front_end.py
@@ -1,3 +1,6 @@
+
+
+
 def data(Codes):
         print("Please select from the following options: list, specific")
         valid = False
@@ -6,24 +9,16 @@ def data(Codes):
             if option == "list":
                 
                 for x, obj in Codes.items():
-                    print(f"{x})
+                    print(x)
 
                 for y in obj:
                     print(y + ':', obj[y])
                 valid = True
             elif (option == "specific"):
+               
                 print("select SIC code")
-                selected_code = input()
-                if selected_code in Codes:
-                    item = Codes[selected_code]
-                    print(f"SIC Code: {selected_code}")
-                    print(f"Description: {item['description']}")
-
-                    #previous code
-                #print("select SIC code")
-                #id = input()
-                #filter(lambda ID: ID['SIC Code'] == id, Codes)
-                
+                id = input()
+                filter(lambda ID: ID['SIC Code'] == id, Codes)
                 valid = True
             else:
                 print("Error: not an option")
--- Lab02/main.py
+++ Lab02/main.py
@@ -1,23 +1,19 @@
 import Front_end
-import import_csv
 
-csvFile = import_csv.read_database("Lab02/catalogue_data.csv")
-
-while True:
-    print("View, Add, or Delete?: ")
-
-    userInput = input()
-
-    if userInput.lower() == "view":
-        Front_end.data(csvFile)
+def checkInput(input):
+    if input.lower() == "view":
+        Front_end.data()
         print("\n--------------------------")
 
-    elif userInput.lower() == "add":
-        #add()
+    elif input.lower() == "add":
+        add()
         print("\n--------------------------")
 
-    elif userInput.lower() == "delete":
-        #delete()
+    elif input.lower() == "delete":
+        delete()
         print("\n--------------------------")
     else:
         print("Invalid input\n--------------------------")
+
+while True:
+    checkInput(input())
