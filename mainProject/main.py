402d61839b815ee99d6e39065731a6ac2b46e6a0
Arian <aryan.vares@gmail.com>
2025-02-28 11:44:28 -0500

    Put together the main program with login and furniture views

--- mainProject/main.py
+++ mainProject/main.py
@@ -1,49 +1,83 @@
-import sqlite3
-import databaseFunctions
+import databasefunction as db
+import furniture
 
-def loginLoop(cursor):
-    permissionLevel = 0
-    while permissionLevel == 0:
+# Main chunk of the program
+def main():
+    # Get the database going
+    connection, cursor = db.loadDatabase()
+    db.initializeDatabase(connection, cursor)
 
-        user = databaseFunctions.attemptLogin(cursor, input("ID Number: "), input("Password: "))
-        if user != 0:
-            permissionLevel = user[1]
-        if permissionLevel > 0:
-            break
-        print("Login Failed.\n")
-    return user
+    # Toss in some test data to play with
+    db.addLogin(connection, cursor, 1, 1, "admin", "pass123")
+    db.addLogin(connection, cursor, 2, 2, "manager", "pass456")
+    db.addFurniture(connection, cursor, 101, "Chair", "Black", 49.99)
+    db.addFurniture(connection, cursor, 102, "Table", "Brown", 99.99)
 
-def inputLoop(connection, cursor, user):
+    # Keep running until they quit
     while True:
-        userInput = input("Add/View/Exit?: ")
-        if userInput == "Add":
-            addFurn(connection, cursor)
-        elif userInput == "View":
-            furniture = databaseFunctions.returnFurniture(cursor)
-            for i in furniture:
-                print(i)
-        elif userInput == "Exit":
-            print("o7")
+        print("\n=== Furniture Inventory Thing ===")
+        print("1. Login")
+        print("2. Get outta here")
+        choice = input("What do you want to do? ")
+
+        if choice == "1":
+            staff_id = input("Staff ID: ")
+            password = input("Password: ")
+            user = db.attemptLogin(cursor, staff_id, password)
+
+            if user:
+                print(f"Hey {user[2]}, you’re in! (Level {user[1]})")
+                logged_in_menu(cursor, user[1])
+            else:
+                print("Wrong ID or password, try again.")
+        
+        elif choice == "2":
+            print("See ya later!")
             break
+        
         else:
-            print("Invalid input.\n")
+            print("Huh? Pick 1 or 2.")
 
-def addFurn(connection, cursor):
-    databaseFunctions.addFurniture(connection, cursor, input("ID: "), input("Type: "), input("Colour: "), input("Price: "))
+    db.closeDatabase(connection)
 
-levels = ["N/A", "Admin"]
-connection, cursor = databaseFunctions.loadDatabase()
+# Menu after they log in
+def logged_in_menu(cursor, user_level):
+    while True:
+        print("\n=== What Now? ===")
+        print("1. See all furniture")
+        print("2. Check one furniture item")
+        print("3. Log out")
+        choice = input("Pick something: ")
 
-#databaseFunctions.initializeDatabase(cursor)
-#databaseFunctions.addLogin(connection, cursor, 100826687, 1, "jaredBatallones", "superSecurePW5")
-#databaseFunctions.addFurniture(connection, cursor, 1, "couch", "green", 440.99)
+        if choice == "1":
+            furniture_list = furniture.get_furniture_list(cursor)
+            if furniture_list:
+                print("\n=== Here’s Everything ===")
+                for item in furniture_list:
+                    print(item)
+            else:
+                print("Nothing in here yet!")
+        
+        elif choice == "2":
+            furniture_id = input("Gimme the furniture ID: ")
+            furniture_list = furniture.get_furniture_list(cursor)
+            found_it = False
+            for item in furniture_list:
+                if str(item.furniture_id) == furniture_id:
+                    print("\n=== Here’s That Item ===")
+                    print(item)
+                    found_it = True
+                    break
+            if not found_it:
+                print("Couldn’t find that one.")
+        
+        elif choice == "3":
+            print("Logging you out now...")
+            break
+        
+        else:
+            print("Pick a real option, c’mon.")
 
-currentLogin = loginLoop(cursor)
-print(f"Welcome {currentLogin[2]}")
-print(f"Permission Level: {levels[currentLogin[1]]}\n")
-
-inputLoop(connection, cursor, currentLogin)
-
-
-
-connection.close()
+# Start the whole thing
+if __name__ == "__main__":
+    main()
